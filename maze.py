65e6ced6d6bf1da9114084a541f2c2051120beb9
Tiffany <tiffany1259@gmail.com>
2021-01-30 05:47:48 -0500

    Revert "make dfs and get neighbors functions"
    This reverts commit fc46cd546f3c34f3f28824ea7d6456716c97de96.

--- maze.py
+++ maze.py
@@ -2,26 +2,6 @@ import pygame
 import random
 import sys
 
-# colors
-BLACK = (50, 50, 50)
-WHITE = (240, 237, 235)
-GREEN = (125, 148, 78)
-RED = (148, 78, 78)
-GREY = (161, 157, 157)
-
-# window
-WINDOW_SIZE = (500, 500)
-screen = pygame.display.set_mode(WINDOW_SIZE)
-pygame.display.set_caption("Fire Maze")
-screen.fill(BLACK)
-
-MARGIN = 1
-dim = 10
-CELL_SIZE = WINDOW_SIZE[0] / dim - 1
-
-# initializes pygame
-pygame.init()
-
 if sys.version_info[0] < 3:
     raise Exception("Python 3 is required for this program.")
 
@@ -80,6 +60,24 @@ def show_maze(_maze: list):
     :param _maze:
     :return: n/a
     """
+    # colors
+    BLACK = (50, 50, 50)
+    WHITE = (240, 237, 235)
+    GREEN = (125, 148, 78)
+    RED = (148, 78, 78)
+
+    # initializes pygame
+    pygame.init()
+
+    # window
+    WINDOW_SIZE = (500, 500)
+    screen = pygame.display.set_mode(WINDOW_SIZE)
+    pygame.display.set_caption("Fire Maze")
+    screen.fill(BLACK)
+
+    MARGIN = 1
+    dim = len(_maze)
+    CELL_SIZE = WINDOW_SIZE[0] / dim - 1
 
     for row in range(dim):
         for col in range(dim):
@@ -121,84 +119,6 @@ def show_maze(_maze: list):
     # update entire display so the rectangles are actually drawn on the screen
     pygame.display.flip()
 
-def get_valid_neighbors(_maze, current, visited):
-  """
-  Get adjacent neighbors which are not an obstacle
-  :param _maze: maze as a grid
-  :param current: current cell
-  :param visited: list of visited cell positions
-  :return: list of neighbors
-  """
-  neighbors = []
-  row = current[0]
-  col = current[1]
-
-  # left
-  if row > 0 and (row - 1, col) not in visited and _maze[row - 1][col] != 1 and _maze[row - 1][col] != 2:
-    neighbors.append((row - 1, col))
-  # right
-  if row + 1 < dim and (row + 1, col) not in visited and _maze[row + 1][col] != 1 and _maze[row + 1][col] != 2:
-    neighbors.append((row + 1, col))
-  # down
-  if col > 0 and (row, col - 1) not in visited and _maze[row][col - 1] != 1 and _maze[row][col - 1] != 2:
-    neighbors.append((row, col - 1))
-  # up
-  if col + 1 < dim and (row, col + 1) not in visited and _maze[row][col + 1] != 1 and _maze[row][col + 1] != 2:
-    neighbors.append((row, col + 1))
-
-  return neighbors
-    
-
-def dfs_search(_maze, start, goal):
-  """
-  Runs dfs on the maze and determines whether the goal is reachable
-  :param _maze: maze as a grid
-  :param start: starting cell
-  :param goal: goal cell
-  :return: true if reachable, false otherwise
-  """
-  visited = []
-
-  # fringe is a list stack
-  fringe = [start]
-  # while fringe is not empty
-  while fringe:
-    current = fringe.pop()
-    visited.append(current)
-
-    # color visited cell except for start and goal
-    if(current != start and current != goal):
-      cell = pygame.Rect((MARGIN + CELL_SIZE) * current[1] + MARGIN,
-                        (MARGIN + CELL_SIZE) * current[0] + MARGIN,
-                        CELL_SIZE,
-                        CELL_SIZE)
-      pygame.draw.rect(screen, GREY, cell)
-
-    if current == goal:
-      visited.append(goal)
-
-      print('\nVisited:')
-      print(visited)
-
-      print('\nElements in fringe:')
-      print(fringe)
-
-      pygame.display.flip()
-      print('\nSUCCESS')
-      return True
-
-    else:
-      neighbors = get_valid_neighbors(_maze, current, visited)
-      fringe.extend(neighbors)
-
-  print('\nVisited:')
-  print(visited)
-
-  print('\nElements in fringe:')
-  print(fringe)
-
-  print('\nFAILED')
-  return False
 
 maze = get_maze()
 print(maze)
@@ -208,8 +128,6 @@ show_maze(maze)
 fired = start_fire(maze)
 print(f"Fire starts: {fired[1]}")
 show_maze(fired[0])
-
-dfs_search(maze, (0, 0), (dim-1, dim-1))
 
 # keep program running until user exits the window
 running = True
