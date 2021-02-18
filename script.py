400939ddf1925bbb60c79c26ef43692e616b3a0d
Tiffany <tiffany1259@gmail.com>
2021-02-17 21:33:02 -0500

    add a* to script

--- script.py
+++ script.py
@@ -7,7 +7,7 @@ import sys
 import time
 import collections
 
-dim = 3000
+dim = 20
 
 if sys.version_info[0] < 3:
     raise Exception("Python 3 is required for this program.")
@@ -120,6 +120,74 @@ def bfs(_maze, start, goal):
     print('\nFAILED')
     return False
 
+def get_distance(start, goal):
+    """
+    Returns the distance between two spots on a grid.
+    :param start:
+    :param goal:
+    :return:
+    """
+    return (goal[0] - start[0]) + (goal[1] - start[1])
+
+def get_distance_from_goal(pos):
+    return get_distance(pos, (dim - 1, dim - 1))
+
+
+class Node:
+    def __init__(self, coords, parent, distance, weight):
+        self.coords = coords
+        self.parent = parent
+        self.distance = distance
+        self.weight = weight
+
+
+def a_star(_maze, start, goal):
+    fringe = collections.deque([Node(start, None, 0, get_distance_from_goal(start))])
+    visited = set()
+    while len(fringe) > 0:
+
+        sorted(fringe, key=lambda x: x.weight)  # Sort by weight
+        current = fringe.popleft()
+
+        if current.coords == goal and (len(fringe) == 0 or fringe[0].weight <= current.weight):
+            print('\nSUCCESS')
+            return True
+        else:
+            neighbors = set()
+            row = current.coords[0]
+            col = current.coords[1]
+
+            # left
+            if row > 0 and (row - 1, col) not in visited and _maze[row - 1][col] != 1 and _maze[row - 1][col] != 2:
+                left_n = (row - 1, col)
+                neighbors.add(left_n)
+                node = Node(left_n, current, current.distance + 1, current.distance + 1 + get_distance_from_goal((left_n[0], left_n[1])))
+                fringe.append(node)
+            # right
+            if row + 1 < dim and (row + 1, col) not in visited and _maze[row + 1][col] != 1 and _maze[row + 1][col] != 2:
+                right_n = (row + 1, col)
+                neighbors.add(right_n)
+                node = Node(right_n, current, current.distance + 1, current.distance + 1 + get_distance_from_goal((right_n[0], right_n[1])))
+                fringe.append(node)
+            # down
+            if col > 0 and (row, col - 1) not in visited and _maze[row][col - 1] != 1 and _maze[row][col - 1] != 2:
+                down_n = (row, col - 1)
+                neighbors.add(down_n)
+                node = Node(down_n, current, current.distance + 1, current.distance + 1 + get_distance_from_goal((down_n[0], down_n[1])))
+                fringe.append(node)
+            # up
+            if col + 1 < dim and (row, col + 1) not in visited and _maze[row][col + 1] != 1 and _maze[row][col + 1] != 2:
+                up_n = (row, col + 1)
+                neighbors.add(up_n)
+                node = Node(up_n, current, current.distance + 1, current.distance + 1 + get_distance_from_goal((up_n[0], up_n[1])))
+                fringe.append(node)
+
+            visited.add(current.coords)
+
+    print('\nFAILED')
+    return False
+
+
 def simulate_dfs(n, p):
     success = 0
     for i in range(n):
@@ -224,7 +292,7 @@ def plot_bfs_astar():
 
 maze = get_maze(0.3)
 start_time = time.time()
-bfs(maze, (0, 0), (dim - 1, dim - 1))
+a_star(maze, (0, 0), (dim - 1, dim - 1))
 print("%s seconds" % (time.time() - start_time))
 
 # plot_dfs()
